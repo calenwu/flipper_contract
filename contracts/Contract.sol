df3ea93450ac5d4197c22f0be4237a5a26f642fc
Georg Ye <calenwuc@gmail.com>
2024-01-20 20:18:30 +0100

    Update Contract.sol
    user can choose seat freely

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -8,19 +8,37 @@ contract Flipper {
     address payable public player1;
     address payable public player2;
     uint256 public betAmount = 0;
+    bool public payer1_is_first_seated;
     uint256 private lastBlock;
 
-    function joinGame() public payable {
+    function joinGame1() public payable {
         require(msg.value > 0, "You must bet some amount.");
-        require(payable(msg.sender) != player1 && payable(msg.sender) != player2, "You cannot sit on both sides of the table.");
-        require(player1 == address(0) || player2 == address(0), "You cannot sit on both sides of the table.");
-        if (player1 == address(0)) {
+        require(player1 == address(0), "Seat 1 is already occupied.");
+        require(payable(msg.sender) != player2, "You cannot sit on both sides of the table.");
+        if (player2 != address(0)) {
+            require(msg.value == betAmount, "You must match the first player's bet.");
+            player1 = payable(msg.sender);
+            lastBlock = block.number;
+        } else {
             betAmount = msg.value;
             player1 = payable(msg.sender);
-        } else {
+            payer1_is_first_seated = true;
+        }
+        emit ContractChanged();
+    }
+
+    function joinGame2() public payable {
+        require(msg.value > 0, "You must bet some amount.");
+        require(player2 == address(0), "Seat 2 is already occupied.");
+        require(payable(msg.sender) != player1, "You cannot sit on both sides of the table.");
+        if (player1 != address(0)) {
             require(msg.value == betAmount, "You must match the first player's bet.");
             player2 = payable(msg.sender);
             lastBlock = block.number;
+        } else {
+            betAmount= msg.value;
+            player2 = payable(msg.sender);
+            payer1_is_first_seated = false;
         }
         emit ContractChanged();
     }
@@ -28,21 +46,31 @@ contract Flipper {
     function getWinner() public {
         require(player1 != address(0) && player2 != address(0), "Need 2 players before getting a winner.");
         require(block.number > lastBlock + 1, "Must wait for block delay before getting a winner");
-        owner.transfer(address(this).balance / 133);  // Send fee to owner
-        if (uint256(blockhash(lastBlock)) % 2 == 0) {
-            emit PaymentMade(player1, player2, address(this).balance);
-            player1.transfer(address(this).balance);
+        uint256 x = uint256(blockhash(lastBlock));
+        if (x == 0) {
+            if (payer1_is_first_seated) {
+                emit PaymentMade(player1, player2, address(this).balance);
+                player1.transfer(address(this).balance);
+            } else {
+                emit PaymentMade(player2, player1, address(this).balance);
+                player2.transfer(address(this).balance);
+            }
         } else {
-            emit PaymentMade(player2, player1, address(this).balance);
-            player2.transfer(address(this).balance);
+            if (x % 2 == 0) {
+                emit PaymentMade(player1, player2, address(this).balance);
+                player1.transfer(address(this).balance);
+            } else {
+                emit PaymentMade(player2, player1, address(this).balance);
+                player2.transfer(address(this).balance);
+            }
         }
         resetGame();
     }
 
     function terminateGame() public {
-        require(
-            (msg.sender == player1 && player2 == address(0)) ||
-            (msg.sender == owner && block.number > lastBlock + 50),
+        require((msg.sender == player1 && player2 == address(0)) ||
+        (msg.sender == player2 && player1 == address(0)) ||
+        (msg.sender == owner && block.number > lastBlock + 50),
             "Only player 1, player 2 or the owner can terminate the game.");
         if (msg.sender == owner) {
             invalidateGame();
